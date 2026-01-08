0b23c70f51099039ceec0581cbf9d118fca7b156
Asuma Yamada <yamadaasuma@gmail.com>
2026-01-08 15:50:03 +0900

    chore: update Neovim configuration and add new plugin management files
    - Introduced .gitignore to exclude fish_variables from version control.
    - Updated fish_variables to include additional user paths for improved environment setup.
    - Added lazy-lock.json and lazyvim.json for managing Neovim plugins with LazyVim.
    - Created new Lua configuration files for coding, editor, LSP, and UI plugins to enhance Neovim functionality and user experience.

--- .config/nvim/lua/plugins/colorschema.lua
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -1,39 +1,43 @@
 return {
-    "Shatur/neovim-ayu",
-    config = function()
-        require("ayu").setup({
-            mirage = true, -- `true` にすると "mirage" バリアントを使用
-            overrides = {
-                Normal = {
-                    bg = "None"
-                },
-                ColorColumn = {
-                    bg = "None"
-                },
-                SignColumn = {
-                    bg = "None"
-                },
-                Folded = {
-                    bg = "None"
-                },
-                FoldColumn = {
-                    bg = "None"
-                },
-                CursorLine = {
-                    bg = "None"
-                },
-                CursorColumn = {
-                    bg = "None"
-                },
-                WhichKeyFloat = {
-                    bg = "None"
-                },
-                VertSplit = {
-                    bg = "None"
-                }
-            }
-        })
-        -- カラースキームを適用
-        vim.cmd("colorscheme ayu-mirage")
-    end
+  "Shatur/neovim-ayu",
+  config = function()
+    require("ayu").setup({
+      mirage = true, -- `true` にすると "mirage" バリアントを使用
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      overrides = {
+        Normal = {
+          bg = "None",
+        },
+        ColorColumn = {
+          bg = "None",
+        },
+        SignColumn = {
+          bg = "None",
+        },
+        Folded = {
+          bg = "None",
+        },
+        FoldColumn = {
+          bg = "None",
+        },
+        CursorLine = {
+          bg = "None",
+        },
+        CursorColumn = {
+          bg = "None",
+        },
+        WhichKeyFloat = {
+          bg = "None",
+        },
+        VertSplit = {
+          bg = "None",
+        },
+      },
+    })
+    -- カラースキームを適用
+    vim.cmd("colorscheme ayu-mirage")
+  end,
 }
