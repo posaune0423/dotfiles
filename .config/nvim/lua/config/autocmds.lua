556eaf0e3ff26a4ee403aa0b80dcd18359dd4f06
Asuma Yamada <yamadaasuma@gmail.com>
2026-01-09 16:52:07 +0900

    fix(autocmds.lua): unbind <C-l> in terminal mode to restore normal clearing behavior and prevent cursor movement to the right window

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -12,3 +12,13 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.conceallevel = 0
   end,
 })
+
+-- Unbind <C-l> in terminal mode to allow normal terminal clearing behavior
+-- Fixes issue where <C-l> moves cursor to right window instead of clearing terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    -- unbind <c-l> in terminal
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", "<C-l>", { noremap = true, silent = true })
+  end,
+})
